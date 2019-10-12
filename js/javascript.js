e7a957a132d20b88829d126769bab439e0e19be4
Alistair Gibson <alistair111@gmail.com>
2019-10-12 16:24:36 +0100

    site ready for deployment

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,16 @@
-// create references to the modal...
 var modal = document.getElementById('myModal');
-// to all images -- note I'm using a class!
+
 var images = document.getElementsByClassName('myImages');
-// the image in the modal
+
 var modalImg = document.getElementById("img01");
-// and the caption in the modal
+
 var captionText = document.getElementById("caption");
 
-// Go through all of the images with our custom class
+
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
-  // and attach our click listener for this image.
-  img.onclick = function(evt) {
+
+  img.onclick = function (evt) {
     console.log(evt);
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -21,6 +20,6 @@ for (var i = 0; i < images.length; i++) {
 
 var span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
